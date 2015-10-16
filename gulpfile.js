da11a5915c3f2d241cf30402b5a0ec835db29e98
Slava Nossar <slavanossar@Slavas-MacBook-Pro.local>
2015-10-17 01:15:53 +1100

    init

---
+++ gulpfile.js
@@ -0,0 +1,187 @@
+/* jshint node: true */
+/* global $: true */
+"use strict";
+
+var gulp = require("gulp");
+var $ = require("gulp-load-plugins")({});
+var rimraf = require("rimraf");
+var envProd = false;
+var runSequence = require('run-sequence');
+var awspublish  = require('gulp-awspublish');
+var staticSrc = "src/**/*.{eot,ttf,woff,woff2,otf,json,pdf}"; // Editable - add any file extensions you want gulp to watch for in /src
+
+require('dotenv').load();
+
+// Clean
+gulp.task("clean", function() {
+	return rimraf.sync("dist");
+});
+
+gulp.task("cacheclear", function() {
+	$.cache.clearAll();
+});
+
+// Copy staticSrc
+gulp.task("copy", function() {
+	return gulp.src(staticSrc, {
+		base: "src"
+	}).pipe( gulp.dest( "dist" ) );
+});
+
+// Compile Partials
+gulp.task('html', function() {
+	gulp.src(['src/*.html'])
+		.pipe($.fileInclude({
+			prefix: '@@',
+			basepath: 'src/partials/'
+		}))
+		.pipe($.minifyHtml({ // Editable - see https://www.npmjs.com/package/gulp-minify-html#options for details
+			conditionals: true;
+		}))
+		.pipe(gulp.dest('dist/'));
+});
+
+// Concatenate JS
+gulp.task("jsconcat", function() {
+	return gulp.src([
+			"bower_components/jquery/dist/jquery.min.js", // Editable - Add any additional paths to JS Bower components here
+			"src/js/vendor/*.js"
+		]).pipe( $.concat("vendor.min.js"))
+		.pipe( gulp.dest("dist/js"));
+});
+
+// JSHint
+gulp.task("jshint", function () {
+	return gulp.src("src/js/*.js")
+		.pipe( $.jshint() )
+		.pipe( $.jshint.reporter( "jshint-stylish" ) )
+		.pipe( $.jshint.reporter('fail') )
+		.on('error', function(e) {
+			if(!envProd) {
+				$.notify().write(e);
+			}
+		});
+});
+
+// Compile JS
+gulp.task( "javascript", ["jshint"], function() {
+	var out = gulp.src([
+			"src/js/plugins/*.js",
+			"src/js/*.js"
+		])
+		.pipe( $.concat( "scripts.min.js" ));
+
+	if(!envProd) {
+		out.pipe($.sourcemaps.init({loadMaps: true}))
+			.pipe($.sourcemaps.write());
+	} else {
+		out.pipe($.uglify());
+	}
+	return out.pipe( gulp.dest( "dist/js" ) );
+});
+
+// Images
+gulp.task("images", function(cb) {
+	return gulp.src('src/img/**/*', {
+		base: "src/img"
+	}).pipe( gulp.dest( "dist/img" ) );
+});
+
+// Stylesheets
+gulp.task("stylesheets", function() {
+	var paths = [
+		'bower_components/normalize-scss/' // Editable - Defines directories where Bower CSS includes can be found. Also make sure to add the usual @import to you main.scss file
+	];
+
+	var out = gulp.src('src/css/main.scss')
+		.pipe( $.sourcemaps.init() )
+		.pipe( $.sass({
+			style: 'expanded',
+			includePaths: paths
+		}))
+		.on('error', $.sass.logError)
+		.on('error', function(e) {
+			if(!envProd) {
+				$.notify().write(e);
+			}
+		})
+		.pipe( $.autoprefixer({
+			browsers: ['last 2 versions'], // Editable - see https://github.com/postcss/autoprefixer#options for details
+			cascade: false
+		})
+	);
+
+	if(!envProd) {
+		out.pipe( $.sourcemaps.write() );
+	} else {
+		out.pipe( $.csso() );
+	}
+
+	return out.pipe( gulp.dest('dist/css') );
+});
+
+// Set Production Environment
+gulp.task( 'production_env', function() {
+	envProd = true;
+});
+
+// Livereload
+gulp.task( "watch", ["stylesheets", "javascript", "jsconcat", "images", "html", "copy"], function() {
+	$.livereload.listen();
+
+	gulp.watch(staticSrc, ["copy"]);
+	gulp.watch("src/**/*.html", ["html"]);
+	gulp.watch("src/js/vendor/*.js", ["jsconcat"]);
+	gulp.watch("src/css/**/*.scss", ["stylesheets"]);
+	gulp.watch("src/js/**/*.js", ["javascript"]);
+	gulp.watch("src/img/**/*.{jpg,png,svg}", ["images"]);
+
+	gulp.watch([
+		"dist/**/*.html",
+		"dist/**/*.js",
+		"dist/**/*.css",
+		"dist/img/**/*"
+	]).on( "change", function( file ) {
+		$.livereload.changed(file.path);
+	});
+});
+
+// Build
+gulp.task( "build", [
+	"production_env",
+	"clean",
+	"stylesheets",
+	"javascript",
+	"jsconcat",
+	"images",
+	"html",
+	"copy"
+], function () {});
+
+// Deploy
+gulp.task( "deploy", function(callback) {
+	runSequence(
+		'build',
+		'publish',
+		 callback)
+});
+
+// Publish to S3
+gulp.task('publish', function() {
+
+	var publisher = awspublish.create({
+			region: 'ap-southeast-2', // Editable - S3 bucket region
+			params: {
+				Bucket: 'example-bucket' // Editable - S3 bucket name
+			},
+			"accessKeyId": process.env.AWS_ACCESS_KEY,
+			"secretAccessKey": process.env.AWS_SECRET_KEY
+		});
+
+	var files = gulp.src(['dist/**'])
+		.pipe(publisher.publish());
+
+	return files
+		.pipe(publisher.cache())
+		.pipe(awspublish.reporter());
+});
---
+++ readme.md
@@ -0,0 +1,75 @@
+# Thirst Quencher
+## A Simple, Satisfying Gulp/Bower Boilerplate That Is Pretty Handy
+
+## Features
+* Compiles HTML partials
+* Compiles and autoprefixes SASS, and will include any `bower_components` CSS/SASS
+* Concatenates JS files and any `bower_components` JS, and runs JSHint
+* Live reload with `watch` task
+* Deploys to S3 bucket with `deploy` task, and will maintain a cache to speed up subsequent deployments
+* Sets production environment on `build` or `deploy` and will minify HTML/CSS and uglify CSS
+
+## Installing Gulp/Bower
+
+Make sure you have `npm` installed (see https://nodejs.org/) and then run
+
+	npm install -g gulp
+	npm install -g bower
+
+## Adding Bower Components and Gulp Packages
+
+You can search for `bower` components by running
+
+	bower search <name>
+
+and Bower will display search results.
+
+To install a `bower` component, run
+
+	bower install --save <name>
+
+from your project directory. The `--save` flag will automatically add the `bower` component to your `bower.json` file.
+
+`gulp` packages are searched for and installed like any other `node` package
+
+	npm search <name>
+	npm install --save <name>
+
+All Gulp packages will have the prefix `gulp-`.
+
+## Installation of packages and components
+
+Run
+
+	npm install
+	bower install
+
+from your project directory to install all the `node` packages and `bower` components.
+
+## Customisation
+
+Comments with the text *Editable* have been added in the `gulpfile.js`, and indicate where you can change/add things to suit your project. Brief comments detailing what can be changed have also been added.
+
+## File/Folder Structure
+
+A basic file/folder structure has been included to illustrate how to organise your `/src` directory.
+
+## Setting Up Deployment
+
+Rename the included `.env.example` file to `.env` and update it with your AWS credentials.
+
+*Note: `.env` should be included in your project's `.gitignore` file so that your credentials will never be included in any commits you make.*
+
+Also make sure to include your S3 bucket `region` and `name` in `gulpfile.js` (locations are marked with the above mentioned *Editable* comments).
+
+## Gulp Tasks
+
+`gulp build` - compiles to /dist (sets production environment and will minify HTML, JS, CSS, etc.)
+`gulp watch` - compiles to /dist (without minification, and will update /dist when you edit files in /src)
+`gulp clean` - deletes /dist
+`gulp cacheclear` - clears gulp's cache (sometimes the `watch` task will have trouble with renamed static files such as images - running this after `gulp clean` and then re-running `gulp build` will fix this)
+`gulp deploy` - will automatically run `gulp build` and then publish to your specified S3 bucket
+
+### Thanks
+
+Special thanks to @jmoggach for first creating this boilerplate. It's been extremely useful to me, and I wanted to share it with others who wanted a quick kand easy way to setup Gulp/Bower for their project.
